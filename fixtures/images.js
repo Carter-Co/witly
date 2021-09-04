ab804e62dfcdae1df35f40e149de059873e380e7
Carter-Co <84072703+Carter-Co@users.noreply.github.com>
2021-09-04 12:39:04 +0800

    Bio and header operational

---
+++ fixtures/images.js
@@ -0,0 +1,22 @@
+export const imageData = [
+    {
+        image: "/images/Ada.png"
+    },
+    {
+        image: "/images/Anita.png"
+    },
+    {
+        image: "/images/Dorothy.png"
+    }, 
+    {
+        image: "/images/Grace.png"
+    },
+    {
+        image: "/images/Katherine.png"
+    },
+    {
+        image: "/images/Mary.png"
+    },
+    {}
+
+]
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import { peopleData } from './fixtures/people.js' 
+import { imageData } from './fixtures/images.js'
+
 
 const app = express();
 app.use(express.static('static'))
 app.set('view engine', 'ejs');
 app.set('views', './views/pages/');
+app.use( express.static( "public" ) );
 
 app.listen(3000, () => {
     console.log('Server started!')
@@ -29,4 +32,5 @@ app.get('/profile/:id', (req, res) => {
     })
     console.log(person);
     res.render('profile', { person: person}); 
+    
 })
